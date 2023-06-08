5e98965401392d90250de25111644a2bab6c2673
Nagy Janos <nagy@zold.lan>
2023-06-08 10:23:12 +0200

    Kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'Alma és körte';
+  name = 'Nagy Ádám';
+
+  employee = {
+    name: 'Erős István',
+    city: 'Szeged',
+    salary: 384
+  }
+
+  employees = [
+    {name: 'Brancs Ádám', city:'Szolnok', salary:397 },
+    {name: 'Rank Ferenc', city:'Miskolc', salary:383 },
+    {name: 'Prima Erik', city:'Budapest', salary:391 },
+    {name: 'Lant Árpád', city:'Szolnok', salary:367 },
+    {name: 'Hisz Laura', city:'Szeged', salary:374 }
+  ];
+
+  constructor() {
+    this.kiir('valami más')
+  }
+  kiir(msg:string):void {
+    console.log(msg)
+    console.log(this.employee.name)
+  }
 }
